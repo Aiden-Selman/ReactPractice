6c54f636c55b2c38876f68b7b847df49564c84e8
Ectectraa <94866347+Aiden-Selman@users.noreply.github.com>
2022-10-23 22:00:26 -0500

    Update ReactPractice.jsx
    Added an example of how to declare a class

---
+++ ReactPractice.jsx
@@ -0,0 +1,8 @@
+// Classes
+// ES6 introduced classes.
+// A class is a type of function, but instead of using the keyword function to initiate it, we use the keyword class, and the properties are assigned inside a constructor() method.
+class Car {
+    constructor(name) {
+      this.brand = name;
+    }
+  }
